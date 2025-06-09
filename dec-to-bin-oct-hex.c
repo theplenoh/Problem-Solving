dfec71a46589f7812547c572ce9687e0aa909520
Plenoh <tylrd529@gmail.com>
2025-06-09 16:59:52 +0900

    Removed func. print_result(), improved func. str_reverse() and func. dec2bin, etc

--- dec-to-bin-oct-hex.c
+++ dec-to-bin-oct-hex.c
@@ -2,83 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define STR_LEN 31
+#define STR_LEN 64
 
-/*
-(hex[0])  = "0000";
-(hex[1])  = "0001";
-(hex[2])  = "0010";
-(hex[3])  = "0011";
-(hex[4])  = "0100";
-(hex[5])  = "0101";
-(hex[6])  = "0110";
-(hex[7])  = "0111";
-(hex[8])  = "1000";
-(hex[9])  = "1001";
-(hex[10]) = "1010";
-(hex[11]) = "1011";
-(hex[12]) = "1100";
-(hex[13]) = "1101";
-(hex[14]) = "1110";
-(hex[15]) = "1111";
-*/
-
-char result[STR_LEN];
-int result_len = 0;
-
-void str_reverse(char*, const int);
-void dec2bin(int);
-void print_result();
+void dec2bin(char* const, int);
+void str_reverse(char* const, const int);
 
 int main()
 {
+    char result[STR_LEN];
+
     int input;
+
+    char hex[16][5];
+
+    strcpy(hex[0], "0000");
+    strcpy(hex[1], "0001");
+    strcpy(hex[2], "0010");
+    strcpy(hex[3], "0011");
+    strcpy(hex[4], "0100");
+    strcpy(hex[5], "0101");
+    strcpy(hex[6], "0110");
+    strcpy(hex[7], "0111");
+    strcpy(hex[8], "1000");
+    strcpy(hex[9], "1001");
+    strcpy(hex[10], "1010");
+    strcpy(hex[11], "1011");
+    strcpy(hex[12], "1100");
+    strcpy(hex[13], "1101");
+    strcpy(hex[14], "1110");
+    strcpy(hex[15], "1111");
 
     printf("input: ");
     scanf("%d", &input);
 
-    dec2bin(input);
-    print_result();
+    dec2bin(result, input);
+
+    puts(result);
 
     return 0;
 }
 
-void dec2bin(int num)
+void dec2bin(char* const str, int number)
 {
-    int cnt = 1;
+    int cnt = 0;
 
-    result[0] = '\0';
+    str[cnt++] = '\0';
 
-    while (num != 0)
+    while (number != 0)
     {
-        result[cnt++] = '0' + (num % 2);
-        num = num / 2;
+        str[cnt++] = '0' + (number % 2);
+        number = number / 2;
     }
 
-    result_len = cnt - 1;
-
-    str_reverse(result, result_len);
+    str_reverse(str, cnt);
 }
 
-void print_result()
+void bin2hex()
 {
+}
+
+// Manually reverse a C-style string
+void str_reverse(char* const str, const int len)
+{
+    char tmp;
+
     int i = 0;
-    while(result[i] != '\0')
-        printf("%c", result[i++]);
+    int j = len - 1;
 
-    printf("\n");
-}
-
-void str_reverse(char* str, const int str_len)
-{
-    int i, len, temp;
-    len = str_len + 1;
-
-    // Swap characters
-    for(i=0; i < len/2; i++)
+    while (i < j)
     {
-        temp = str[i];
-        str[i] = str[len-i-1];
-        str[len-i-1] = temp;
+        tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+
+        i++;
+        j--;
     }
 }
