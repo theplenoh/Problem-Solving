717151f148c153cf6eda5e717487f6a33accfbed
Plenoh <tylrd529@gmail.com>
2025-10-08 16:57:35 +0900

    Add a factorial example using recursion

---
+++ fac_recur.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fac_recur(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n * fac_recur(n - 1);
+}
+
+int main()
+{
+    int input;
+
+    scanf("%d", &input);
+
+    printf("%d\n", fac_recur(input));
+
+    return 0;
+}
